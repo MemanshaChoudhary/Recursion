2dcc92eafb76accedea62bcf0ce1dc86c6754a47
Memansha Choudhary <memanshachoudhary2403@gmail.com>
2023-06-11 21:34:38 +0530

    Update capitalize.java

--- capitalize.java
+++ capitalize.java
@@ -1,16 +1,24 @@
 //Write recursive function that takes an array of words and returns an array containing all words capitalized
-public class capitalize {
-    static String[] capital(String [] a, int i){
-        if(i==a.length){
-            return a;
-        }
-       a.split();
+import java.util.Arrays;
 
-}
-    
+public class CapitalizingWords {
+    public static String[] capitalize(String[] words, int index) {
+        if (index < 0 || index >= words.length) {
+            return new String[0];
+        } else {
+            String word = words[index];
+            String capitalizedWord = word.toUpperCase();
+            String[] remaining = capitalize(words, index + 1);
+            String[] result = new String[remaining.length + 1];
+            result[0] = capitalizedWord;
+            System.arraycopy(remaining, 0, result, 1, remaining.length);
+            return result;
+        }
+    }
+
     public static void main(String[] args) {
-        String [] a={"foo", "bar"};
-         String []ans = capital(a,0);
-         System.out.println(ans);
+        String[] words = {"hello", "world", "bye-bye"};
+        String[] capitalizedWords = capitalize(words, 0);
+        System.out.println("Capitalized words: " + Arrays.toString(capitalizedWords));
     }
 }
